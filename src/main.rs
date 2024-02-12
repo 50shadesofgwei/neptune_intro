94ca09d55dbaa95edaf212c78a1c7d8e5952c84a
50 <jonofeasby4@gmail.com>
2024-02-12 12:05:02 +0000

    add quote test in main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
+use std::error::Error;
+use utils::global_types::AssetToSellData;
+use crate::request_for_quote::zero_ex::get_request_from_asset_data::ZeroExQuoter;
+
 mod request_for_quote;
 mod utils;
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let rfq_object: AssetToSellData = AssetToSellData {
+        sell_token_address: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2".to_string(),
+        buy_token_address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48".to_string(),
+        sell_token_amount: "1000000000000000000".to_string(),
+    };
+
+    let quoter: ZeroExQuoter = ZeroExQuoter::new()?;
+    let quote: request_for_quote::zero_ex::zero_ex_types::ZeroExAPIResponseData = quoter.get_zero_ex_quote_from_asset_data(rfq_object).await?;
+
+    println!("{:?}", quote);
+
+    Ok(())
 }
--- src/request_for_quote/zero_ex/get_request_from_asset_data.rs
+++ src/request_for_quote/zero_ex/get_request_from_asset_data.rs
@@ -1,19 +1,25 @@
-use std::error::Error;
 use reqwest::{self, Client, Response};
+use serde::Deserialize;
+use std::{error::Error};
 use dotenv::dotenv;
 use std::env;
 use crate::utils::global_types::AssetToSellData;
 use super::{zero_ex_utils::ZERO_EX_API_ADDRESS, zero_ex_types::ZeroExAPIResponseData};
+
+#[derive(Debug, Deserialize)]
+pub struct ZeroExAPIError {
+    pub message: String,
+}
 
 pub struct ZeroExQuoter {
     api_key: String,
 }
 
 impl ZeroExQuoter {
-    pub fn new() -> Result<Self, Box<dyn std::error::Error>> {
+    pub fn new() -> Result<Self, Box<dyn Error>> {
         dotenv().ok();
         let api_key: String = env::var("ZERO_EX_API_KEY")
-            .map_err(|e| format!("Couldn't read ZERO_EX_API_KEY from .env ({})", e))?;
+        .map_err(|e| format!("Couldn't read ZERO_EX_API_KEY from .env ({})", e))?;
 
         Ok(Self {
             api_key,
@@ -34,9 +40,18 @@ impl ZeroExQuoter {
             ])
             .header("0x-api-key", &self.api_key)
             .send()
-            .await?;
-        let zero_ex_api_response: ZeroExAPIResponseData = res.json::<ZeroExAPIResponseData>().await?;
+            .await
+            .map_err(|e| format!("Network error: {}", e))?;
+
+        if !res.status().is_success() {
+            let error_message: String = res.text().await.unwrap_or_else(|_| "Unknown error".to_string());
+            return Err(format!("Error response from ZeroEx API: {}", error_message).into());
+        }
+
+        let zero_ex_api_response: ZeroExAPIResponseData = res.json::<ZeroExAPIResponseData>().await
+            .map_err(|e| format!("Error deserializing response: {}", e))?;
 
         Ok(zero_ex_api_response)
     }
 }
+
